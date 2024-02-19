237f5f590a4c18db455bf399de2d23af7e55088d
Michael Kraus <michael.kraus@mapp.com>
2024-02-19 11:38:00 +0100

    feat: add scss version for iconfont styles

--- .fantasticonrc.js
+++ .fantasticonrc.js
@@ -1,13 +1,14 @@
 module.exports = {
   inputDir: './src/icons', // (required)
   outputDir: './.fantasticon', // (required)
-  assetTypes: ['css', 'html', 'ts'],
+  assetTypes: ['css', 'scss', 'html', 'ts'],
   name: 'mapp-fonts',
   prefix: 'mui-icons',
   selector: '.mui-icons',
   // Use a custom Handlebars template
   templates: {
     css: './src/templates/css.hbs',
+    scss: './src/templates/scss.hbs',
     html: './src/templates/html.hbs'
   }
 };
